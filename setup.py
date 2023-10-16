9dde66daeb44147533d5174a46e049f852924aef
taylorchu <tailinchu@gmail.com>
2023-10-16 10:35:14 -0700

    disable triton

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ def read_version(fname="whisper/version.py"):
 
 
 requirements = []
-#if sys.platform.startswith("linux") and platform.machine() == "x86_64":
-#    requirements.append("triton==2.0.0")
 
 setup(
     name="openai-whisper",
